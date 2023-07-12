7b3e966fadc4a1e879f508c1cb8d30142cf36cc5
Rishabh Sinha <sinharishabh402@gmail.com>
2023-07-12 13:33:55 +0530

    main

--- src/utils/apiController.js
+++ src/utils/apiController.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = "http://localhost:3000/";
+const baseURL = "https://rococo-manatee-1b0b7d.netlify.app/";
 
 const getAllTodo = (setNotes) => {
     axios
