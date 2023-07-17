9af9e37907eb991e64dfbf569aa0b51be5b0c855
Rishabh Sinha <sinharishabh402@gmail.com>
2023-07-17 15:10:13 +0530

    Commit

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import './styles.css'
 
+alert('There are few demo notes. Feel free to delete them and get Started.');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
